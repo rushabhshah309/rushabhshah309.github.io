8249a79e6517ab7d1238560da92dd33087ace2d4
unknown <shahrushabh309@gmail.com>
2022-10-02 15:34:50 -0700

    initial commit

---
+++ forms/contactForm.php
@@ -0,0 +1,64 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'shahrushabh309@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+
+
+  // $contact = new PHP_Email_Form;
+  // $contact->ajax = true;
+  
+  // $contact->to = $receiving_email_address;
+  // $contact->from_name = $_POST['name'];
+  // $contact->from_email = $_POST['email'];
+  // $contact->subject = $_POST['subject'];
+  $to = $receiving_email_address;
+  $name = $_POST['name'];
+  $email_address = $_POST['email'];
+  $message = $_POST['subject'];
+  
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  
+  // $contact->smtp = array(
+  //   'host' => 'example.com',
+  //   'username' => 'example',
+  //   'password' => 'pass',
+  //   'port' => '587'
+  // );
+  
+  
+  // $contact->add_message( $_POST['name'], 'From');
+  // $contact->add_message( $_POST['email'], 'Email');
+  // $contact->add_message( $_POST['message'], 'Message', 10);
+  
+  // $contact->send();
+
+  // $to = $receiving_email_address;
+
+  $email_subject = "Contact form submission: $name";
+  
+  $email_body = "You have received a new message. ".
+  
+  " Here are the details:\n Name: $name \n ".
+  
+  "Email: $email_address\n Message \n $message";
+  
+  $headers = "From: $myemail\n";
+  
+  $headers .= "Reply-To: $email_address";
+  
+  mail($to,$email_subject,$email_body,$headers);
+
+?>
